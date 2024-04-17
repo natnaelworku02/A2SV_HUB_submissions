3c5fce4af70c49ee63ebf781583d4caa62731d9a
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-17 17:27:49 +0300

    Created 'Find K Closest Elements' by A2SV Hub

---
+++ 17-Apr-2024/Find K Closest Elements 125628.py
@@ -0,0 +1,16 @@
+# Problem: Find K Closest Elements - https://leetcode.com/problems/find-k-closest-elements/
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        for i in range(len(arr)):
+            arr[i] = [abs(x-arr[i]),arr[i]]
+        heapify(arr)
+        # print(arr)
+        ans = []
+        for i in range(k):
+            _,val = heappop(arr)
+            ans.append(val)
+
+        return sorted(ans)
+        
+        
