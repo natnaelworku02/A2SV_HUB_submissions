a4b1ca1a48c0b8f523d2c49355df89cdb7d71686
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-04-19 19:06:52 +0300

    Created 'Course Schedule II' by A2SV Hub

---
+++ 19-Apr-2024/Course Schedule II 128080.py
@@ -0,0 +1,30 @@
+# Problem: Course Schedule II - https://leetcode.com/problems/course-schedule-ii/description/
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        indeg = [0] * numCourses
+        adjlist = defaultdict(list)
+
+        for u ,v in prerequisites:
+            adjlist[v].append(u)
+            indeg[u] += 1
+        
+        queue = deque()
+        for i , count in enumerate(indeg):
+            if count == 0:
+                queue.append(i)
+        # print(queue)
+        ans = []
+        count = 0
+        while queue:
+            for i in range(len(queue)):
+                node = queue.popleft()
+                ans.append(node)
+                count += 1
+                for ind in adjlist[node]:
+                    indeg[ind] -= 1
+                    if indeg[ind] == 0:
+                        queue.append(ind)
+        # if 
+        return ans if count == numCourses else []
