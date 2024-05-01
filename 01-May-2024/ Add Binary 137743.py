d23edfeaeaa40291833839f98c30cd3893f84690
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-01 09:25:59 +0300

    Created ' Add Binary' by A2SV Hub

---
+++ 01-May-2024/ Add Binary 137743.py
@@ -0,0 +1,36 @@
+# Problem:  Add Binary - https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = list(map(int,a))[::-1]
+        b = list(map(int,b))[::-1]
+        carry = 0
+
+        if len(a ) > len(b):
+            b.extend([0 for _ in range(len(a) - len(b))])
+        else:
+            a.extend([0 for _ in range(len(b) - len(a))])
+        
+        # print(a)
+        # print(b)
+        # return ""
+        ans = []
+        for i in range(len(a)):
+            val = a[i] + b[i] + carry
+            if val== 2:
+                ans.append(0)
+                carry = 1
+            elif val == 3:
+                ans.append(1)
+                carry = 1
+            else:
+                ans.append(val)
+                carry = 0
+        if carry: ans.append(carry)
+
+        return ''.join(map(str,ans[::-1]))
+
+
+
+
+        
