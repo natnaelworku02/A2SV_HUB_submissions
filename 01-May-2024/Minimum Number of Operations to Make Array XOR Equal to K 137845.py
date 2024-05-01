07ff60fb0790f97a5a2cb70314fb1a3114f8c9d8
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-01 09:52:26 +0300

    Created 'Minimum Number of Operations to Make Array XOR Equal to K' by A2SV Hub

---
+++ 01-May-2024/Minimum Number of Operations to Make Array XOR Equal to K 137845.py
@@ -0,0 +1,11 @@
+# Problem: Minimum Number of Operations to Make Array XOR Equal to K - https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        final_xor = 0
+        for n in nums:
+            final_xor = final_xor ^ n
+        
+        
+
+        return bin(final_xor ^ k).count("1")
