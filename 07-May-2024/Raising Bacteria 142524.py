716ef2a37522847b822047f59889ee4ac6ca6370
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-07 17:23:07 +0300

    Created 'Raising Bacteria' by A2SV Hub

---
+++ 07-May-2024/Raising Bacteria 142524.py
@@ -0,0 +1,11 @@
+# Problem: Raising Bacteria - https://codeforces.com/contest/579/problem/A
+
+n = int(input())
+ans = 0
+
+while n>1:
+    if n % 2:
+        ans += 1
+    n //=2
+print(ans+1)
+    
