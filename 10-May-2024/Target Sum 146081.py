3a88406e636796526d89c23e19e0ebcaffe5d847
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-10 19:05:37 +0300

    Created 'Target Sum' by A2SV Hub

---
+++ 10-May-2024/Target Sum 146081.py
@@ -0,0 +1,19 @@
+# Problem: Target Sum - https://leetcode.com/problems/target-sum/
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+        def dp(ind,sumed):
+            if ind == len(nums):
+                if sumed == target:
+                    return 1
+                return 0
+            
+            if (ind,sumed) not in memo:
+                ans = dp(ind+1 , sumed + nums[ind])
+                ans += dp(ind+1,sumed - nums[ind])
+                memo[(ind,sumed)] = ans
+            
+            return memo[(ind,sumed)]
+        
+        return dp(0,0)
