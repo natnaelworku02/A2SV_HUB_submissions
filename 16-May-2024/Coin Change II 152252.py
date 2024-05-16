26615e10cd0ed7e1eac1d5bb99bdfec4bc61cced
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-16 18:45:43 +0300

    Created 'Coin Change II' by A2SV Hub

---
+++ 16-May-2024/Coin Change II 152252.py
@@ -0,0 +1,17 @@
+# Problem: Coin Change II - https://leetcode.com/problems/coin-change-ii/
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+        memo  = [0]*(amount+1)
+        memo[0] = 1
+
+        for i in range(len(coins)):
+            for j in range(len(memo)):
+                if j + coins[i] < len(memo):
+                    memo[j + coins[i]] += memo[j]
+            # print(memo)
+            # print("________")
+        
+        return memo[-1]
+
