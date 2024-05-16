a4eebf4566774b65ca4ea3f8f6a56f89d8826d19
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-16 18:22:59 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 16-May-2024/Unique Paths 152199.py
@@ -0,0 +1,21 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = [1  for _ in range(n)]
+        # print(memo)
+        for i in range(1,m):
+            cur =  [0  for _ in range(n)]
+            for j in  range(n):
+                cur[j] += memo[j]
+                # print(cur[j],memo[j])
+                if j - 1 > -1:
+                    cur[j] += cur[j-1]
+            
+            # print(cur)
+            
+            memo = cur
+                
+
+        
+        return memo[-1]
