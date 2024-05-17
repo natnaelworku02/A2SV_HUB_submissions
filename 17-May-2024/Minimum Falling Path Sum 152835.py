ed3c358f163665e155cdc9c7f1e5ee8b64621dea
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-17 10:08:33 +0300

    Created 'Minimum Falling Path Sum' by A2SV Hub

---
+++ 17-May-2024/Minimum Falling Path Sum 152835.py
@@ -0,0 +1,20 @@
+# Problem: Minimum Falling Path Sum - https://leetcode.com/problems/minimum-falling-path-sum/
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        
+        for i in range(1,len(matrix)):
+            for j in range(len(matrix[0])):
+
+                val = inf
+                if i - 1 > - 1:
+                    val = min(val,matrix[i-1][j])
+                if i - 1  > - 1 and j - 1 > - 1:
+                    val = min(val,matrix[i-1][j-1])
+                if i - 1  >-1  and j + 1 < len(matrix[0]):
+                    val = min(val,matrix[i-1][j+1])
+                # print(val,i,j)
+                matrix[i][j] += val
+        
+        return min(matrix[-1])
+
