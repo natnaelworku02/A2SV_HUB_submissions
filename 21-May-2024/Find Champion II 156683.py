1b49b33f2b8e4137df7049011c1cee88170abcbd
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-21 19:16:47 +0300

    Created 'Find Champion II' by A2SV Hub

---
+++ 21-May-2024/Find Champion II 156683.py
@@ -0,0 +1,18 @@
+# Problem: Find Champion II - https://leetcode.com/problems/find-champion-ii/
+
+class Solution:
+    def findChampion(self, n: int, edges: List[List[int]]) -> int:
+        
+        adjlist = defaultdict(list)
+        num ={i:0 for i in range(n)}
+        for u,v in edges:
+            adjlist[u].append(v)
+            num[v] += 1
+        # print(num)
+        # count = 0
+        ans = []
+        for key,val in num.items():
+            if val ==0:
+                ans.append(key)
+        
+        return ans[0] if len(ans) == 1 else -1
