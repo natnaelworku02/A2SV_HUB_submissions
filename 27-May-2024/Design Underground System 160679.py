eedafc01176191e74f3ac6fffc82191b00966b4e
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-27 22:01:23 +0300

    Created 'Design Underground System' by A2SV Hub

---
+++ 27-May-2024/Design Underground System 160679.py
@@ -0,0 +1,33 @@
+# Problem: Design Underground System - https://leetcode.com/problems/design-underground-system/
+
+class UndergroundSystem:
+
+    def __init__(self):
+        self.checkedIn={}
+        self.checkedOut=defaultdict(int)
+        self.count=defaultdict(int)
+        
+
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.checkedIn[id]=[t,stationName]
+        
+
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        self.checkedOut[(self.checkedIn[id][1],stationName)]+=t-self.checkedIn[id][0]
+        self.count[(self.checkedIn[id][1],stationName)]+=1
+        self.checkedIn.pop(id)
+        
+
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        data_set=(startStation,endStation)
+        if data_set in self.checkedOut:
+            return self.checkedOut[data_set]/self.count[data_set]
+
+        
+
+
+# Your UndergroundSystem object will be instantiated and called as such:
+# obj = UndergroundSystem()
+# obj.checkIn(id,stationName,t)
+# obj.checkOut(id,stationName,t)
+# param_3 = obj.getAverageTime(startStation,endStation)
