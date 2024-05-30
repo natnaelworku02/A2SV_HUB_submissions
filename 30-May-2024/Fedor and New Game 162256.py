585ac46ffd79dccff21f315c3bc7a2f6c8402054
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-05-30 18:04:04 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 30-May-2024/Fedor and New Game 162256.py
@@ -0,0 +1,16 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n,m,k  = list(map(int,input().split()))
+arr = []
+
+for _ in range(m+1):
+    arr.append(int(input()))
+ans = 0
+for i in range(m):
+    val =(arr[i] ^ arr[-1])
+    val = bin(val)
+    val = val[2:]
+    if val.count('1') <= k:
+        ans+=1
+# print(bin(~(26 ^ 18)),26 ^ 18, ~(26 ^ 18))
+print(ans)
