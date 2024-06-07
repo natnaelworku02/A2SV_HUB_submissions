7df491090a56ede5ed6ec0734a4ff564e8dce892
Natigold2002 <104618033+Natigold2002@users.noreply.github.com>
2024-06-07 22:52:04 +0300

    Created 'Max Numbers of K sums' by A2SV Hub

---
+++ 07-Jun-2024/Max Numbers of K sums 165621.py
@@ -0,0 +1,20 @@
+# Problem: Max Numbers of K sums - https://leetcode.com/problems/max-number-of-k-sum-pairs
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l=0
+        r=len(nums) - 1
+        count =0
+        while l<r:
+            sumed = nums[l] + nums[r]
+            if sumed < k:
+                l+=1
+
+            elif sumed > k:
+                r-=1
+            else:
+                count+=1
+                l+=1
+                r-=1
+        return count
