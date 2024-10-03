3dd480c4655471e1e38e4135831ce8ee4ee97371
Natnael Worku <104618033+natnaelworku02@users.noreply.github.com>
2024-10-03 10:48:01 +0300

    Created 'Sum of Digits of String After Convert' by A2SV Hub

---
+++ 03-Oct-2024/Sum of Digits of String After Convert 217684.py
@@ -0,0 +1,24 @@
+# Problem: Sum of Digits of String After Convert - https://leetcode.com/problems/sum-of-digits-of-string-after-convert/
+
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        
+        nums = []
+
+        for ch in s:
+            # print(ord(ch), ord(ch) - 96, ch)
+            val = str(ord(ch) - 96)
+            val = [i for i in val]
+            nums.extend(val)
+        
+        # ans = 0
+        # print(nums)
+        for i in range(k):
+            temp = 0
+            for val in nums:
+                temp += int(val)
+            # print(temp)
+            temp = str(temp)
+            nums = [i for i in temp]
+        
+        return int(''.join(nums))
