0ac3bc440f9442df8cace5f704463eedb90c9865
Natnael Worku <104618033+natnaelworku02@users.noreply.github.com>
2024-10-25 19:06:20 +0300

    Created 'Group Anagrams' by A2SV Hub

---
+++ 25-Oct-2024/Group Anagrams 222128.py
@@ -0,0 +1,21 @@
+# Problem: Group Anagrams - https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+            di = defaultdict(list)
+
+            for i,s in enumerate(strs):
+                s = list(s)
+                s.sort()
+                s = tuple(s)
+                di[s].append(i)
+            ans = []
+            for value in di.values():
+                temp = []
+                for val in value:
+                    temp.append(strs[val])
+                
+                ans.append(temp)
+            
+            return ans
